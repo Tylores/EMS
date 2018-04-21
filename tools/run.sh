8417b47c8139ddb46e45b0db663952dc9959226f
Tylores <tylor.slay@gmail.com>
2018-04-21 13:35:16 -0700

    removed the DER dummy agents from the EMS code to clean it up.

--- tools/run.sh
+++ tools/run.sh
@@ -8,6 +8,12 @@ export ALLJOYN_ROOT=$ROOTPATH/alljoyn
 export AJ_ROOT=$ALLJOYN_ROOT
 export LD_LIBRARY_PATH=$AJ_ROOT/build/linux/$CPU/$VARIANT/dist/cpp/lib:$LD_LIBRARY_PATH
 
-#assume testRun.sh has already been ran
+#run alljoyn router
+killall alljoyn-daemon
+sleep 1
+$AJ_ROOT/build/linux/$CPU/$VARIANT/dist/cpp/bin/alljoyn-daemon --config-file=./rn-config.xml &
+
+#run app
+make -C ../cpp
 ./../cpp/bin/Debug/EMS
 
